3cba3a7272bc237310f1cd6f478c3c04343c189b
Sam Won <swon.epsilon@gmail.com>
2020-04-19 15:44:17 -0700

    Add User Resource Fix 1

--- app.py
+++ app.py
@@ -5,22 +5,29 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
-from resources.user import UserRegister, User
+from resources.user import UserRegister, User, UserList
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
+# from db import db
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') # 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = '4ckf4fm2'
 api = Api(app)
 
+# @app.before_first_request
+# def create_tables():
+# 	db.create_all()
+
 jwt = JWT(app, authenticate, identity) # /auth
 
 # items = []
 
-api.add_resource(UserRegister, '/register').
+api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserList, '/users')
 
@@ -31,6 +38,7 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__': # this prevents app.run() from running when app.py is imported from other files.
+	# db.init_app(app)
 	app.run(port=5000, debug=True) 
 
 # Common Status Code List
--- resources/user.py
+++ resources/user.py
@@ -37,14 +37,14 @@ class UserRegister(Resource):
 class User(Resource):
 	@classmethod
 	def get(cls, user_id):
-		user = UserModel.find_by_username(user_id)
+		user = UserModel.find_by_id(user_id)
 		if not user:
 			return {'message': 'User not found'}, 404
 		return user.json()
 	
 	@classmethod
 	def delete(cls, user_id):
-		user = UserModel.find_by_username(user_id)
+		user = UserModel.find_by_id(user_id)
 		if not user:
 			return {'message': 'User not found'}, 404
 		user.delete_user()
