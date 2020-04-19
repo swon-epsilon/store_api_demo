2bba708436ff75966d4f9181181601be3245bcf0
Sam Won <swon.epsilon@gmail.com>
2020-04-19 16:22:26 -0700

    Add JWT Extended Fix 1

--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask_restful import Resource, reqparse
-from flask_jwt import JWT, jwt_required
+from flask_jwt_extended import jwt_required
+
 from models.item import ItemModel
 
 class Item(Resource):
