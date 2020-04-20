e3b1ebab56bb8e0dfc5c554e07fd87fd34e8bc76
Sam Won <swon.epsilon@gmail.com>
2020-04-19 17:38:50 -0700

    Add JWT Identity

--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,6 @@
 import sqlite3
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import jwt_required, jwt_optional, get_jwt_identity, get_jwt_claims
 
 from models.item import ItemModel
 
@@ -99,9 +99,16 @@ class Item(Resource):
 
 
 class ItemList(Resource):
-	@jwt_required
+	@jwt_optional
 	def get(self):
-		return {'items': [item.json() for item in ItemModel.find_all()]}
+		user_id = get_jwt_identity()
+		items = [item.json() for item in ItemModel.find_all()]
+
+		if user_id:
+			return {'items': items}, 200
+
+		return {'items': [item['name'] for itme in items],
+				'message': 'More data available if you log in'}, 200
 		# return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
 
 		# connection = sqlite3.connect('data.db')
